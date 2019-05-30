42cf9a0b432afee94db2968c33937a0e12c8f028
Andrew Cecil <atcecil@gmail.com>
2019-05-30 19:54:07 -0400

    Add fullscreen menu view, start JS file, and add event listener for menu button

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+// Create Menu button variable
+var menuButton = document.getElementById('menuButton')
+var menuOpen = false;
+var nav = getElementById('nav')
+
+// Create button event to toggle navigation menu view
+menuButton.addEventListener("click", function() {
+  // Test button functionality
+  console.log("Button test confirmed ")
+  // Toggle menu display
+  if (menuOpen === false) {
+    // add code to make #nav display viewable
+
+  } else {
+    // add code to make #nav display dissappear
+    
+  }
+})
