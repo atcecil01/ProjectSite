7ceb56e3c653109792a2a14fb59ae1bf36a6ccf8
Andrew Cecil <atcecil@gmail.com>
2019-06-02 08:22:20 -0400

    Added JS to toggle menu on/off in large view - error with menu in small view after toggling.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 // Create Menu button variable
 var menuButton = document.getElementById('menuButton')
 var menuOpen = false;
-var nav = getElementById('nav')
+var nav = document.getElementById('nav')
 
 // Create button event to toggle navigation menu view
 menuButton.addEventListener("click", function() {
@@ -11,9 +11,12 @@ menuButton.addEventListener("click", function() {
   // Toggle menu display
   if (menuOpen === false) {
     // add code to make #nav display viewable
-
+    nav.style.display = 'block';
+    menuOpen = true;
   } else {
     // add code to make #nav display dissappear
-    
+    // resizing after toggling menu results in no menu showing ////////////
+    nav.style.display = 'none';
+    menuOpen = false;
   }
 })
