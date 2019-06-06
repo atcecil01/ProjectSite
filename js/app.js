56939b7cc123c6661539e096917afac85e90cd0f
Andrew Cecil <atcecil@gmail.com>
2019-06-06 19:39:30 -0400

    Added additional html pages for projects, and added event listener to check screen width

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 var menuButton = document.getElementById('menuButton')
 var menuOpen = false;
 var nav = document.getElementById('nav')
+var sWidth = window.screen.width;
+console.log("Initial Width: " + sWidth);
+
+
 
 // Create button event to toggle navigation menu view
 menuButton.addEventListener("click", function() {
@@ -18,5 +22,36 @@ menuButton.addEventListener("click", function() {
     // resizing after toggling menu results in no menu showing ////////////
     nav.style.display = 'none';
     menuOpen = false;
-  }
+  };
+})
+
+
+
+
+// Add event listener to check screen width
+window.addEventListener("resize", function() {
+  sWidth=window.screen.width;
+  console.log(sWidth);
+  if (sWidth < 1027) {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
+    menuOpen = false;
+    // Create button event to toggle navigation menu view
+    menuButton.addEventListener("click", function() {
+      // Test button functionality
+      console.log("Button test confirmed")
+      // Toggle menu display
+      if (menuOpen === false) {
+        // add code to make #nav display viewable
+        nav.style.display = 'block';
+        menuOpen = true;
+      } else {
+        // add code to make #nav display dissappear
+        // resizing after toggling menu results in no menu showing ////////////
+        nav.style.display = 'none';
+        menuOpen = false;
+      }
+    })
+  };
 })
