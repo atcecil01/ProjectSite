91a229ad7d811b8ec1c641070757a219d6bfc28b
Andrew Cecil <atcecil@gmail.com>
2019-06-20 19:44:24 -0400

    Added jQuery and created image carousel for woodcraft page

--- js/woodApp.js
+++ js/woodApp.js
@@ -1,2 +1,29 @@
 // Image Carousel
 
+// Query Selectors
+var imgDisplay = document.querySelector('.img-display');
+var prev = document.querySelector('prev');
+var next = document.querySelector('next');
+var index = 0;
+
+//Image Array
+var imgArray = ['./img/tea1.PNG','./img/tea4.JPG','./img/tea5.JPG','./img/tea6.JPG','./img/tea8.JPG','./img/tea9.JPG','./img/tea10.JPG'];
+
+$('.prev').click(function() {
+    if (index > 0) {
+        console.log("jQuery load successfull")
+        imgDisplay.src = imgArray[index - 1];
+        index -= 1;
+        console.log(index);
+    };
+});
+
+
+$('.next').click(function() {
+    if (index < (imgArray.length - 1)) {
+        console.log("jQuery load successfull")
+        imgDisplay.src = imgArray[index + 1];
+        index += 1;
+        console.log(index);
+    };
+});
